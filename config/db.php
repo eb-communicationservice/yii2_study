951d400a6393383b48d6cf054c9572fbd0951ea8
横谷美遥 <yokotani.miharu@ditgroup.jp>
2016-02-11 21:57:59 +0900

    ・チュートリアル３_データベース

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'myoko',
+    'password' => 'yo5ta2',
     'charset' => 'utf8',
 ];
